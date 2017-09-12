9f69d4c8c41f7b5b78e2d29914e1c889ec3ee1a0
Alexey Nikitin <nikialeksey@gmail.com>
2017-09-13 00:28:43 +0700

    Refactor

--- rms-core/src/test/java/com/hydrogenious/rms/stub/ReferenceTermsApiStubTest.java
+++ rms-core/src/test/java/com/hydrogenious/rms/stub/ReferenceTermsApiStubTest.java
@@ -2,12 +2,11 @@ package com.hydrogenious.rms.stub;
 
 import com.hydrogenious.rms.model.ReferenceTerm;
 import org.hamcrest.core.Is;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collections;
 import java.util.Set;
-
-import static org.junit.Assert.*;
 
 public class ReferenceTermsApiStubTest {
 
@@ -15,6 +14,6 @@ public class ReferenceTermsApiStubTest {
     public void findAllReferenceTerms() {
         final ReferenceTermsApiStub referenceTermsApi = new ReferenceTermsApiStub(Collections.emptySet());
         final Set<ReferenceTerm> allReferenceTerms = referenceTermsApi.findAllReferenceTerms();
-        assertThat(allReferenceTerms, Is.is(Collections.emptySet()));
+        Assert.assertThat(allReferenceTerms, Is.is(Collections.emptySet()));
     }
 }
