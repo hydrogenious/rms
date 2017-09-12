90295ea039314b50909eea58aecb7031ee815512
Alexey Nikitin <nikialeksey@gmail.com>
2017-09-13 00:03:00 +0700

    Added codecov and stub test

---
+++ rms-core/src/test/java/com/hydrogenious/rms/stub/ReferenceTermsApiStubTest.java
@@ -0,0 +1,20 @@
+package com.hydrogenious.rms.stub;
+
+import com.hydrogenious.rms.model.ReferenceTerm;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class ReferenceTermsApiStubTest {
+    @Test
+    public void findAllReferenceTerms() {
+        final ReferenceTermsApiStub referenceTermsApi = new ReferenceTermsApiStub(Collections.emptySet());
+        final Set<ReferenceTerm> allReferenceTerms = referenceTermsApi.findAllReferenceTerms();
+        assertThat(allReferenceTerms, Is.is(Collections.emptySet()));
+    }
+
+}
